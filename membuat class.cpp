5f5dd0aa3772210f1648e7759048abe8c132b236
AdrianMochtar <mochtaradrian.am@gmail.com>
2018-01-19 22:10:28 +0800

    buat outputnya

--- membuat class.cpp
+++ membuat class.cpp
@@ -6,3 +6,23 @@ class uang{
 	public:
 		string warna,nmpahlawan;
 };
+
+int main(){
+	uang duarb,limarb;
+	
+	duarb.warna = "abu-abu";
+	duarb.nmpahlawan = "MOHAMMAD HOESNI THAMRIN";
+	
+	limarb.warna = "coklat";
+	limarb.nmpahlawan = "Dr.K.H.IDHAM CHALID";
+	
+	cout << "Ciri-ciri uang Rp.2000" <<endl;
+	cout << "warna uangnya adalah = "<<(duarb.warna)<<endl;
+	cout << "nama pahlawannya adalah = "<<(duarb.nmpahlawan)<<endl;
+	
+	cout <<endl;
+	
+	cout << "Ciri-ciri uang Rp.5000" <<endl;
+	cout << "warna uangnya adalah = "<<(limarb.warna)<<endl;
+	cout << "nama pahlawannya adalah = "<<(limarb.nmpahlawan)<<endl;
+}
